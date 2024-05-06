f29161dd194b707c2d7ff84e3f4391a02bd53b03
MZIO <mziodev@gmail.com>
2024-05-06 17:27:23 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by MZIO on 6/5/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+        Text("Second screen!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
