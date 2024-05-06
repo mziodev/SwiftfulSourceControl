ebbc89119a65a23706806aee2716dc0d23b78fa1
MZIO <mziodev@gmail.com>
2024-05-06 17:38:11 +0200

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by MZIO on 6/5/24.
 //
 
+/*
+ Commit Messages
+ 
+ New feature:
+ [Feature] Description of the feature
+ 
+ Bug in production
+ [Patch] Description of patch
+ 
+ Bug not in production
+ [Bug] Description of the bug
+ 
+ Mundane tasks
+ [Clean] Description of changes
+ 
+ Release:
+ [Release] Description o releases
+ 
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
