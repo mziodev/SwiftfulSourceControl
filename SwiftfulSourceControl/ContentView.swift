a5b38de7eb74979dff1577b0ec0bf093f00986dc
MZIO <mziodev@gmail.com>
2024-05-07 10:27:48 +0200

    [Feature] Update Click me button to Subscribe Now

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
                 
             }
             
-            Button("Click me!") {
+            Button("Subcribe Now!") {
                 
             }
         }
